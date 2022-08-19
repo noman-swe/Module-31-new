6cce45142ffb3e4d93d363406fdfd63143dbfb7a
noman-swe <noman.swe130@gmail.com>
2022-08-19 16:58:52 +0600

    31-1 Simple Recap of JavaScript six core parts

---
+++ By-Toutorial/recap.js
@@ -0,0 +1,43 @@
+// variable
+var deposit = 400;
+// conditions and conditional checkers
+if (deposit > 500) {
+
+}
+else if (deposit == 300) {
+
+}
+else {
+
+}
+
+// array
+const numbers = [55, 65, 95, 81, 58];
+const numbersCount = numbers.length;
+console.log(numbersCount);
+
+numbers.pop();
+numbers.push(111);
+numbers[2] = 9999;
+console.log(numbers);
+
+if (numbers.indexOf(444) == -1) {
+    // 
+}
+if (numbers.includes(333)) {
+    // it will return true or false means checks true or false
+}
+
+// basic function
+
+function fullName(first, last){
+const name = first + last;
+}
+const person = fullName('Rakib', 'Sakin');
+
+// object
+const bottole = {
+    color: 'yellow',
+    price: 200,
+    usagesFor: 'water'
+}
