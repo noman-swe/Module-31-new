8c314127ba0e93d7d79c19a1bcbf6ef894e173d7
noman-swe <noman.swe130@gmail.com>
2022-08-21 00:27:49 +0600

    31-6 Arrow function, multiple parameter, function body

---
+++ By-Toutorial/arrow.js
@@ -0,0 +1,27 @@
+// function declaretion system
+function add1 (num1, num2){
+    return num1 + num2;
+}
+// const sum1 = add1(10, 13);
+// console.log(sum1);
+
+// function expression
+const add2 = function add2(num1, num2){
+    return num1 + num2;
+}
+const sum2 = add2(10, 13);
+console.log(sum2);
+
+
+// function expression  but anonymous function
+const add3 = function(num1, num2){
+    return num1 + num2;
+}
+const sum3 = add2(10, 13);
+console.log(sum3);
+
+
+// 
+const add4 = (num1, num2) => num1 + num2;
+const sum4 = add4(2, 5);
+console.log(sum4);
